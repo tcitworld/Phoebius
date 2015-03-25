0a4dca69437f0d33de81161a6a75660381f27297
Augier <contact@c-henry.fr>
2015-03-25 15:27:34 +0100

    Interface for SongList

--- app/src/main/groovy/augier/fr/phoebius/utils/SongList.groovy
+++ app/src/main/groovy/augier/fr/phoebius/utils/SongList.groovy
@@ -6,31 +6,29 @@ import android.database.Cursor
 import android.net.Uri
 import android.provider.MediaStore
 
-class SongList
+class SongList implements ISongList
 {
-	private final String SONG_TITLE = MediaStore.Audio.Media.TITLE
-	private final String SONG_ID = MediaStore.Audio.Media._ID
-	private final String SONG_ARTIST = MediaStore.Audio.Media.ARTIST
-	private final Uri MUSIC_URI = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
-
 	private ContentResolver musicResolver
-	private ArrayList<Song> songList = []
-	private int currentSong
+	private ArrayList<Song> currSongList = []
 	private Cursor musicCursor
+	private long currentSongId
+
+	private boolean loop
 
 	public SongList(ContentResolver _musicResolver)
 	{
 		musicResolver = _musicResolver
-		currentSong = 0
 		musicCursor = queryCursor
+		currentSongId = songList[1].ID
+		loop = false
 	}
 
 	public ArrayList<Song> getSongList()
 	{
-		songList.empty
+		currSongList.empty
 		musicCursor = queryCursor
 
-		if(musicCursor!=null && musicCursor.moveToFirst())
+		if(musicCursor != null && musicCursor.moveToFirst())
 		{
 			int titleColumn = songTitleColumn
 			int idColumn = songIdColumn
@@ -41,19 +39,17 @@ class SongList
 				long thisId = it.getLong(idColumn)
 				String thisTitle = it.getString(titleColumn)
 				String thisArtist = it.getString(artistColumn)
-				songList.add(new Song(thisId, thisTitle, thisArtist))
+				currSongList.add(new Song(thisId, thisTitle, thisArtist))
 			}
 			*/
-
 
 			while(musicCursor.moveToNext())
 			{
 				long thisId = musicCursor.getLong(idColumn)
 				String thisTitle = musicCursor.getString(titleColumn)
 				String thisArtist = musicCursor.getString(artistColumn)
-				songList.add(new Song(thisId, thisTitle, thisArtist))
+				currSongList.add(new Song(thisId, thisTitle, thisArtist))
 			}
-			
 
 
 		}
@@ -62,14 +58,41 @@ class SongList
 
 	public ArrayList<Song> sort()
 	{
-		songList.sort({ song1, song2 ->  return song1.artist.compareTo(song2.artist) })
-		return songList
+		currSongList.sort({ song1, song2 ->  return song1.artist.compareTo(song2.artist) })
+		return currSongList
 	}
 
-	public Song getAt(int idx){ return songList[idx] }
+	public Song getAt(int idx){ return currSongList[idx] }
+
+	@Override
+	public Song getNextSong()
+	{
+		int _nextSongIdx = nextSongIdx
+		if(_nextSongIdx < 0){ return null}
+		else{ return currSongList[_nextSongIdx] }
+	}
+
+	@Override
+	public Song getPreviousSong()
+	{
+		return null
+	}
+
+	//region PRIVATE METHODS
+	private int findIndexById(id){ return currSongList.findIndexOf{ it.ID == id } }
+	private Song findById(id){ return currSongList[findIndexById(id)] }
+	private int getNextSongIdx()
+	{
+		int currentSongIdx = findIndexById(currentSongId)
+		if(currentSongIdx == currSongList.size() - 1){ return -1 }
+		else{ return currentSongIdx + 1 % currSongList.size() }
+	}
+	//endregion
 
 	//region GET/SET
-	public int getLenght(){ return songList.size() }
+	public int getLenght(){ return currSongList.size() }
+	public boolean getLoop(){ return loop }
+	public void setLoop(boolean loop){ this.loop = loop }
 	private int getSongTitleColumn(){ return musicCursor.getColumnIndex(SONG_TITLE) }
 	private int getSongIdColumn(){ return musicCursor.getColumnIndex(SONG_ID) }
 	private int getSongArtistColumn(){ return musicCursor.getColumnIndex(SONG_ARTIST) }
